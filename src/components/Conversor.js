fdf8740192c9326c59912f3eb0c45e870ed0e859
Leonardo Merbold <leonardo.merbold@gmail.com>
2022-10-04 21:36:43 -0300

    Graph fix and Code improvements

--- src/components/Conversor.js
+++ src/components/Conversor.js
@@ -4,17 +4,6 @@ import { DateTime } from 'luxon';
 import { Chart as ChartJS } from 'chart.js/auto';
 import {Line} from 'react-chartjs-2';
 
-// const [userData, setUserData] = useState({
-//     labels: UserData.map((data) => data.year),
-//     datasets: [{
-//       label: "Toletão",
-//       data: UserData.map((data) => data.userGain),
-//       backgroundColor: ["green"],
-//       borderColor: "black",
-//       borderWidth: 1,
-//     }]
-//   })
-
 export default class Conversor extends Component{
 
     constructor(props) {
@@ -32,136 +21,128 @@ export default class Conversor extends Component{
         this.season = this.season.bind(this);
     }
 
-    converter() {
+
+    async converter() {
         const from_to = `${this.state.coinA_type}${this.state.coinB_type}`
         const url = `https://economia.awesomeapi.com.br/json/last/${this.state.coinA_type}-${this.state.coinB_type}`
 
-        fetch(url)
-        .then(res=>{
-            return res.json()
-        }).then(json=>{
-            const quotation = json[from_to].bid;
+        const quotation = await fetch(url)
+            .then((e) => e.json())
+            .then((e) => e[from_to].bid);
 
-            // if (this.state.coinA_value === "") {
-            //     this.setState({ coinB_value:'' });
-            // } else {
+        if (this.state.coinA_value === "") {
+            this.setState({ coinB_value:'' });
+        } else {
             const coinB_value = (parseFloat(this.state.coinA_value) * quotation).toFixed(2)
             this.setState({ coinB_value })
-            // }
-        })
+        }
     }
 
     season(graph) {
-        let dateEnd = DateTime.now().setZone("system");
-        let dateStart = 0, dateAmount = 0, diff = 0, infoData = [];
-        //console.log(this.state.graph);
-        //console.log(this.dateEnd);
 
-        switch(graph){
-            case '1D':
-                dateStart = dateEnd.minus({ days: 1 });
-                dateAmount  = 24;
-                dateStart = dateStart.toFormat('yyyyMMdd');
-                dateEnd = dateEnd.toFormat('yyyyMMdd');
-                break;
-            case '3D':
-                dateStart = dateEnd.minus({ days: 3 });
-                dateAmount = 48
-                dateStart = dateStart.toFormat('yyyyMMdd');
-                dateEnd = dateEnd.toFormat('yyyyMMdd');
-                break;
-            case '7D':
-                dateStart = dateEnd.minus({ days: 7 });
-                dateAmount = 28;
-                dateStart = dateStart.toFormat('yyyyMMdd');
-                dateEnd = dateEnd.toFormat('yyyyMMdd');
-                break;
-            case '15D':
-                dateStart = dateEnd.minus({ days: 15 });
-                dateAmount = 30;
-                dateStart = dateStart.toFormat('yyyyMMdd');
-                dateEnd = dateEnd.toFormat('yyyyMMdd');
-                break;
-            case '1M':
-                dateStart = dateEnd.minus({ month: 1 });
-                diff = dateEnd.diff(dateStart, 'days');
-                dateAmount = diff.values.days;
-                dateStart = dateStart.toFormat('yyyyMMdd');
-                dateEnd = dateEnd.toFormat('yyyyMMdd');
-                 break;
-            case '3M':
-                dateStart = dateEnd.minus({ month: 3 });
-                diff = dateEnd.diff(dateStart, 'days');
-                dateAmount = diff.values.days;
-                dateStart = dateStart.toFormat('yyyyMMdd');
-                dateEnd = dateEnd.toFormat('yyyyMMdd');
-                break;
-             case '5M':
-                dateStart = dateEnd.minus({ month: 5 });
-                diff = dateEnd.diff(dateStart, 'days');
-                dateAmount = diff.values.days;
-                dateStart = dateStart.toFormat('yyyyMMdd');
-                dateEnd = dateEnd.toFormat('yyyyMMdd');
-                break;
-            case '1A':
-                dateStart = dateEnd.minus({ year: 1 });
-                diff = dateEnd.diff(dateStart, 'days');
-                dateAmount = diff.values.days;
-                dateStart = dateStart.toFormat('yyyyMMdd');
-                dateEnd = dateEnd.toFormat('yyyyMMdd');
-                break;
-            case '5A':
-                dateStart = dateEnd.minus({ year: 5 });
-                diff = dateEnd.diff(dateStart, 'days');
-                dateAmount = diff.values.days;
-                dateStart = dateStart.toFormat('yyyyMMdd');
-                dateEnd = dateEnd.toFormat('yyyyMMdd');
-                break;
-            case 'Max':
-                console.log("Máximo");
-                break;
-            default:
-                console.log('default');
+        try {
+            let dateEnd = DateTime.now().setZone("system");
+            let dateStart = 0, dateAmount = 0, graphColor1 = '', graphColor2 = '';
+
+            switch(graph){
+                case '1D':
+                    dateStart = dateEnd.minus({ days: 1 });
+                    dateAmount  = 24;
+                    dateStart = dateStart.toFormat('yyyyMMdd');
+                    dateEnd = dateEnd.toFormat('yyyyMMdd');
+                    graphColor1 = 'rgb(160,194,0)';
+                    graphColor2 = 'rgba(160,194,0,0.2)';
+                    break;
+                case '3D':
+                    dateStart = dateEnd.minus({ days: 3 });
+                    dateAmount = 48
+                    dateStart = dateStart.toFormat('yyyyMMdd');
+                    dateEnd = dateEnd.toFormat('yyyyMMdd');
+                    graphColor1 = 'rgb(213,187,250)';
+                    graphColor2 = 'rgba(213,187,250,0.2)';
+                    break;
+                case '7D':
+                    dateStart = dateEnd.minus({ days: 7 });
+                    dateAmount = 28;
+                    dateStart = dateStart.toFormat('yyyyMMdd');
+                    dateEnd = dateEnd.toFormat('yyyyMMdd');
+                    graphColor1 = 'rgb(245,164,51)';
+                    graphColor2 = 'rgba(245,164,51,0.2)';
+                    break;
+                case '15D':
+                    dateStart = dateEnd.minus({ days: 15 });
+                    dateAmount = 15;
+                    dateStart = dateStart.toFormat('yyyyMMdd');
+                    dateEnd = dateEnd.toFormat('yyyyMMdd');
+                    graphColor1 = 'rgb(247,60,41)';
+                    graphColor2 = 'rgb(247,60,41,0.2)';
+                    break;
+                case '1M':
+                    dateStart = dateEnd.minus({ month: 1 });
+                    dateAmount = dateEnd.diff(dateStart, 'days').values.days;
+                    graphColor1 = 'rgb(95,255,76)';
+                    graphColor2 = 'rgba(95,255,76,0.2)';
+                    break;
+                case '3M':
+                    dateStart = dateEnd.minus({ month: 3 });
+                    dateAmount = dateEnd.diff(dateStart, 'days').values.days;
+                    graphColor1 = 'rgb(41,186,227)';
+                    graphColor2 = 'rgb(41,186,227,0.2)';
+                    break;
+                case '6M':
+                    dateStart = dateEnd.minus({ month: 6 });
+                    dateAmount = dateEnd.diff(dateStart, 'days').values.days;
+                    graphColor1 = 'rgb(251,194,45)';
+                    graphColor2 = 'rgb(251,194,45,0.2)';
+                    break;
+                case '1A':
+                    dateStart = dateEnd.minus({ year: 1 });
+                    dateAmount = dateEnd.diff(dateStart, 'days').values.days;
+                    graphColor1 = 'rgb(247,126,126)';
+                    graphColor2 = 'rgba(247,126,126,0.2)';
+                    break;
+                case '5A':
+                    dateStart = dateEnd.minus({ year: 5 });
+                    dateAmount = dateEnd.diff(dateStart, 'days').values.days;
+                    break;
+                case 'Max':
+                    console.log("Máximo");
+                    break;
+                default:
+                    console.log('default');
+            }
+
+            //fetch(`https://economia.awesomeapi.com.br/USD-BRL/${dateAmount}?start_date=${dateStart}&end_date=${dateEnd}`)
+            fetch(`https://economia.awesomeapi.com.br/json/daily/${this.state.coinA_type}-${this.state.coinB_type}/${dateAmount}`)
+                .then( (e) => e.json()
+                    .then( data => {
+                        const resData = ( data.map((e) => e.high ));
+                        const APIGraph = {
+                            labels: data.reverse().map((e) => DateTime.fromSeconds(Number(e.timestamp)).toFormat('dd-MM-yyyy')),
+                            datasets: [{
+                                label: 'Conversão',
+                                fill: true,
+                                lineTension: 0,
+                                backgroundColor: graphColor2,
+                                borderColor: graphColor1,
+                                hoverBackgroundColor: 'rgb(0,0,0)',
+                                borderWidth: 2,
+                                data: resData.reverse()
+                            }]
+                        }
+                        this.setState({APIGraph});
+                    })
+                )
+
+        } catch(error) {
+            console.log(error);
         }
-
-        //const loopData = [];
-
-        //fetch(`https://economia.awesomeapi.com.br/USD-BRL/${dateAmount}?start_date=${dateStart}&end_date=${dateEnd}`)
-        fetch(`https://economia.awesomeapi.com.br/json/daily/USD-BRL/5`)
-        .then( res => {
-            //console.log(res);
-            res.json()
-            .then(data => {
-                //console.log(data.map((e) => DateTime.fromSeconds(Number(e.timestamp)).toISO()))
-                infoData = (data.map((e) => e.high))
-                console.log("Resultados das altas tragas da API", infoData);
-            })
-        }).catch((error) => {
-            console.log("Erro na promise");
-        });
-
-        const APIGraph = {
-            labels: ['January', 'February', 'March',
-                    'April', 'May'],
-            datasets: [{
-                label: 'Conversão',
-                fill: false,
-                lineTension: 0,
-                backgroundColor: 'rgba(31,240,75,1)',
-                borderColor: 'rgba(31,240,75,1)',
-                borderWidth: 2,
-                //data: [infoData]
-            }]
-        }
-        this.setState({APIGraph});
-        console.log(APIGraph.datasets.data);
     }
 
     render() {
         const options = this.props.options;
         const listOfSiglas = Object.keys(options);
 
-        //console.log(listOfSiglas);
         return (
             <div id="application">
 
@@ -170,11 +151,9 @@ export default class Conversor extends Component{
                         value={this.state.coinA_value}
                         type="number"
                         min="0"
-                        // onKeyUp={this.converter}
                         onInput={
                             (event) => {
                                 let valorInput = event.target.value;
-                                //valorInput = valorInput.replace(/^[a-zA-Z!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/, '');
                                 this.setState({ coinA_value: valorInput});
                                 this.converter();
                             }
@@ -217,7 +196,7 @@ export default class Conversor extends Component{
 
                         <button onClick={() => { this.season('3M') }}>3M</button>
 
-                        <button onClick={() => { this.season('5M') }}>5M</button>
+                        <button onClick={() => { this.season('6M') }}>6M</button>
 
                         <button onClick={() => { this.season('1A') }}>1A</button>
 
@@ -233,17 +212,17 @@ export default class Conversor extends Component{
                         data={this.state.APIGraph}
                         // options={{
                         //     title:{
-                        //     display:true,
-                        //     text:'Average Rainfall per month',
-                        //     fontSize:20,
+                        //         display:false,
+                        //         text:'Average Rainfall per month',
+                        //         fontSize:20,
                         //     },
                         //     legend:{
-                        //     display:true,
-                        //     position:'right'
+                        //         display:true,
+                        //         position:'right'
                         //     }
                         // }}
                     />)
-                    : <h3> Errou! </h3>}
+                    : <h4> Aguardando Gráfico... </h4>}
                 </div>
             </div>
         )
